a6d22b29bb22f29c9af5396a5f53b97191c15cba
20171105134 <s20171105134@admindeiMac-32.local>
2018-10-12 09:27:54 +0800

    operate is worng

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -9,7 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var flag = 0
+    var temp = 0
+    var result = 0
+    @IBAction func setZero(_ sender: Any) {
+        couterDisplay.text = ""
+    }
+    
+    @IBAction func digitButton(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        couterDisplay.text = couterDisplay.text! + digit
+    }
+    
+    @IBOutlet weak var couterDisplay: UITextField!
+    
+    @IBAction func getDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        couterDisplay.text = couterDisplay.text! + digit
+    }
+    
+    @IBAction func operate(_ sender: UIButton) {
+        let operation = sender.currentTitle!
+        
+        temp = couterDisplay.text
+        switch operation {
+        case "+": performOperation{$0 + $1}
+        
+        default:break
+            <#code#>
+        }
+    }
+    
+   
+    @IBAction func operateCharacter5(_ sender: Any) {
+        if flag == 1{
+            result = temp +  Int(couterDisplay.text!)!
+        }
+        else if flag == 2{
+            result = temp - Int(couterDisplay.text!)!
+        }
+        else if flag == 3{
+            result = temp * Int(couterDisplay.text!)!
+        }
+        else if flag == 4{
+            result = temp / Int(couterDisplay.text!)!
+        }
+        couterDisplay.text = "\(result)"
 
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
