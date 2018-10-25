a14d77048e11da6f2c90da4e86762b0c6bfd0871
20171105134 <s20171105134@admindeiMac-32.local>
2018-10-25 10:04:44 +0800

    sum can accumulation

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     var flag = 0
     var temp = 0
+    var buffer = 0
     var result = 0
    
    
@@ -27,41 +28,50 @@ class ViewController: UIViewController {
     @IBOutlet weak var couterDisplay: UITextField!
     
     @IBAction func plus(_ sender: Any) {
+        buffer = temp + Int(couterDisplay.text!)!
         temp = Int(couterDisplay.text!)!
         flag = 1
         couterDisplay.text = ""
     }
     @IBAction func subtract(_ sender: Any) {
+        buffer = temp - Int(couterDisplay.text!)!
         temp = Int(couterDisplay.text!)!
         flag = 2
         couterDisplay.text = ""
+        
     }
     @IBAction func mult(_ sender: Any) {
+        
+        buffer = Int(couterDisplay.text!)! * temp
+        temp = 1
         temp = Int(couterDisplay.text!)!
         flag = 3
         couterDisplay.text = ""
     }
     @IBAction func division(_ sender: Any) {
+        buffer = Int(couterDisplay.text!)! / temp
+        temp = 1
         temp = Int(couterDisplay.text!)!
         flag = 4
         couterDisplay.text = ""
     }
+   
     
     @IBAction func operate(_ sender: Any) {
         if flag == 1{
-            result = temp +  Int(couterDisplay.text!)!
+            result = buffer +  Int(couterDisplay.text!)!
         }
         else if flag == 2{
             
-            result = temp - Int(couterDisplay.text!)!
+            result = buffer - Int(couterDisplay.text!)!
         }
         else if flag == 3{
             
-            result = temp * Int(couterDisplay.text!)!
+            result = buffer * Int(couterDisplay.text!)!
         }
         else if flag == 4{
             
-            result = temp / Int(couterDisplay.text!)!
+            result = Int(couterDisplay.text!)! / buffer
             
         }
         couterDisplay.text = "\(result)"
