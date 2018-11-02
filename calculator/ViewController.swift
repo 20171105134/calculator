e0fddd6ab3dd90414b96b23008bfa4ba2d956791
20171105134 <s20171105134@admindeiMac-32.local>
2018-11-02 09:59:12 +0800

    let division cannot realize summation

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     var flag = 0
     var temp : Double = 0
-    var secoungImport : Double = 0
+    var secoundImport : Double = 1
     var fristImport : Double = 0
     var buffer : Double = 0
     var result :Double = 0
@@ -21,6 +21,7 @@ class ViewController: UIViewController {
     
     @IBAction func setZero(_ sender: Any) {
         couterDisplay.text = ""
+        buffer = 0
     }
     
     @IBAction func digitButton(_ sender: UIButton) {
@@ -37,31 +38,41 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var couterDisplay: UITextField!
     
-    @IBAction func plus(_ sender: Any) {
+    @IBAction func plusAnthorone(_ sender: Any) {//fristImport = Double(couterDisplay.text!)!
+        //if fristImport != 0{
+            buffer = fristImport + Double(couterDisplay.text!)!
+        //}
+        //secoundImport = buffer
+        //buffer = temp + Double(couterDisplay.text!)!
+        //temp = Double(couterDisplay.text!)!
         fristImport = Double(couterDisplay.text!)!
-        buffer = temp + Double(couterDisplay.text!)!
-        temp = Double(couterDisplay.text!)!
         couterDisplay.text = ""
+        flag = 1
     }
     @IBAction func subtract(_ sender: Any) {
-        buffer = temp - Double(couterDisplay.text!)!
-        temp = Double(couterDisplay.text!)!
+        
+        //if fristImport != 0{
+            buffer = fristImport + (-Double(couterDisplay.text!)!)
+        //}
+        //buffer = temp - Double(couterDisplay.text!)!
+        //buffer = Double(couterDisplay.text!)!
+        fristImport = Double(couterDisplay.text!)!
         flag = 2
         couterDisplay.text = ""
         
     }
     @IBAction func mult(_ sender: Any) {
-        
-        buffer = Double(couterDisplay.text!)! * temp
-        temp = 1
-        temp = Double(couterDisplay.text!)!
+        //fristImport = 1
+        //fristImport = buffer
+        buffer = Double(couterDisplay.text!)! * secoundImport
+        secoundImport = Double(couterDisplay.text!)!
         flag = 3
         couterDisplay.text = ""
     }
     @IBAction func division(_ sender: Any) {
-        buffer = temp/Double(couterDisplay.text!)!
+        buffer = (1/secoundImport) * Double(couterDisplay.text!)!
     
-        temp = Double(couterDisplay.text!)!
+        secoundImport = buffer
         flag = 4
         couterDisplay.text = ""
     }
@@ -69,24 +80,24 @@ class ViewController: UIViewController {
     
     @IBAction func operate(_ sender: Any) {
         if flag == 1{
-            result = temp +  Double(couterDisplay.text!)!
+            result = buffer +  Double(couterDisplay.text!)!
         }
         else if flag == 2{
             
-            result = temp - Double(couterDisplay.text!)!
+            result = buffer - Double(couterDisplay.text!)!
         }
         else if flag == 3{
             
-            result = temp * Double(couterDisplay.text!)!
+            result = buffer * Double(couterDisplay.text!)!
         }
         else if flag == 4{
             
-            result = temp/Double(couterDisplay.text!)!
+            result = buffer/Double(couterDisplay.text!)!
             
         }
         couterDisplay.text = "\(result)"
-        print(temp)
-        print(plus)
+        //print(temp)
+        //print(plus)
     }
 
     
